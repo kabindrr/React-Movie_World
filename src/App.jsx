8e8d1630351d808f40761e80425b0286d1adcd5f
kabindrr <chaina_kabindra@yahoo.com>
2024-07-05 14:18:12 +1000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      Hellow from developers entry point
+      <Button className="btn btn-danger">Search movie</Button>
+    </>
+  );
+};
+
+export default App;
