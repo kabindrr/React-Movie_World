c4105764d529f986ee5a985bdc23ec146630301d
Kabindra Ranabhat <chaina_kabindra@yahoo.com>
2024-07-09 13:05:23 +1000

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiKEY = import.meta.env.VITE_APIKEY;
-const apiEP = `http://www.omdbapi.com/?apikey=${apiKEY}&`;
+const apiEP = `https://www.omdbapi.com/?apikey=${apiKEY}&`;
 export const fetchFromAPI = async (str) => {
   try {
     const url = apiEP + "t=" + str;
